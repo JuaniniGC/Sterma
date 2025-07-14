6a0fa3754fb647d39991539bf1f7a78252b7e3f5
JuaniniGC <juangarciacarballo@gmail.com>
2025-07-15 01:04:33 +0200

    fix: add dto to return values

---
+++ back/src/main/java/com/sterma/back/dtos/auth/login/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.sterma.back.dtos.auth.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+
+}
