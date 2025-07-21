9766a8a980cadb7869ff5ae5fe00b502c5cc0387
Juan Garcia Carballo <juangarciacarballo@gmail.com>
2025-07-21 19:31:00 +0200

    feat: add list all elevator from one community back

--- back/src/main/java/com/sterma/back/controller/CommunityController.java
+++ back/src/main/java/com/sterma/back/controller/CommunityController.java
@@ -2,10 +2,14 @@ package com.sterma.back.controller;
 
 
 import com.sterma.back.models.Community;
+import com.sterma.back.models.Elevator;
 import com.sterma.back.repositories.CommunityRepository;
+import com.sterma.back.repositories.ElevatorRepository;
 import com.sterma.back.services.CommunityService;
+import com.sterma.back.services.ElevatorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +21,20 @@ public class CommunityController {
 
     private final CommunityService communityService;
 
-    public CommunityController(CommunityService communityService) {
+    private final ElevatorService elevatorService;
+
+    public CommunityController(CommunityService communityService, ElevatorService elevatorService) {
         this.communityService = communityService;
+        this.elevatorService = elevatorService;
     }
 
     @GetMapping
     public List<Community> getAll(){
         return communityService.listAll();
+    }
+
+    @GetMapping("/{id}/elevator")
+    public List<Elevator> getElevator(@PathVariable Long id){
+        return elevatorService.listByElevatorId(id);
     }
 }
