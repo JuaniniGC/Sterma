f20f31f0d0c0934d3d6581f62a6cd2f16c4d35fd
JuaniniGC <juangarciacarballo@gmail.com>
2025-11-10 17:09:17 +0100

    fix: fix builder for incident report

--- back/src/main/java/com/sterma/back/models/reports/IncidentReport.java
+++ back/src/main/java/com/sterma/back/models/reports/IncidentReport.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @Setter
+@SuperBuilder
 @Table(name = "incident_report")
 public class IncidentReport extends Report{
 }
